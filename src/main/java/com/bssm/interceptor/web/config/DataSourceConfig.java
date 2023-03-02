99cef863268abf6685e028950944d02cbd1516a1
youngmin <min050410@gmail.com>
2023-03-02 18:45:27 +0900

    init: spring 기본 설정 및 hikari pool config

---
+++ src/main/java/com/bssm/interceptor/web/config/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.bssm.interceptor.web.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.hikari.jdbc-url}")
+    private String jdbcUrl;
+    @Value("${spring.datasource.hikari.username}")
+    private String username;
+    @Value("${spring.datasource.hikari.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+
+}
