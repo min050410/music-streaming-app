29e4196e7553b34b3ad2ff3a8583d6a765f580b3
min050410 <min050410@gmail.com>
2023-05-09 23:52:51 +0900

    feat(Song): enum custom validator 추가

---
+++ src/main/java/com/bssm/interceptor/common/decorator/validator/EnumValidator.java
@@ -0,0 +1,29 @@
+package com.bssm.interceptor.common.decorator.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Enum value, ConstraintValidatorContext context) {
+        boolean result = false;
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue: enumValues) {
+                if (value == enumValue) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+}
