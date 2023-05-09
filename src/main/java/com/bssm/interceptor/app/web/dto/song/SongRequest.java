c1d2b571f7034026605fa55010b9d4f957c67275
min050410 <min050410@gmail.com>
2023-05-10 00:02:37 +0900

    feat(Song): toSong 메서드 작성

--- src/main/java/com/bssm/interceptor/app/web/dto/song/SongRequest.java
+++ src/main/java/com/bssm/interceptor/app/web/dto/song/SongRequest.java
@@ -1,6 +1,9 @@
 package com.bssm.interceptor.app.web.dto.song;
 
 import com.bssm.interceptor.app.domain.enums.SongGenreType;
+import com.bssm.interceptor.app.domain.member.Member;
+import com.bssm.interceptor.app.domain.song.Song;
+import com.bssm.interceptor.common.decorator.validator.ValidEnum;
 import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,7 +18,18 @@ public class SongRequest {
     @NotBlank
     private String name;
 
-    @NotBlank
+    @ValidEnum(enumClass = SongGenreType.class)
     private SongGenreType genre;
+
+    public Song toSong(Member member) {
+        // TODO: file 로직 추가시 수정
+        return Song.builder()
+            .name(name)
+            .file("hi")
+            .length(20L)
+            .songGenreType(genre)
+            .member(member)
+            .build();
+    }
 
 }
