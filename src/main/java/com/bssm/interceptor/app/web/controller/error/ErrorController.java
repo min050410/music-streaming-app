fcf9ce1f4d088c28deda01efec5b8565a36fd46d
min050410 <min050410@gmail.com>
2023-05-15 12:12:06 +0900

    feat(Error): error controller 메소드 수정

--- src/main/java/com/bssm/interceptor/app/web/controller/error/ErrorController.java
+++ src/main/java/com/bssm/interceptor/app/web/controller/error/ErrorController.java
@@ -3,7 +3,8 @@ package com.bssm.interceptor.app.web.controller.error;
 import com.bssm.interceptor.common.exception.JwtException;
 import com.bssm.interceptor.app.web.path.ApiPath;
 import io.swagger.v3.oas.annotations.Hidden;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ErrorController {
 
-    @GetMapping(ApiPath.ERROR_AUTH)
+    @RequestMapping(
+        value = ApiPath.ERROR_AUTH,
+        method = {
+            RequestMethod.GET,
+            RequestMethod.DELETE,
+            RequestMethod.PUT,
+            RequestMethod.POST
+        }
+    )
     public void errorAuth(@RequestParam(value = "message") String message) {
         throw new JwtException(message);
     }
